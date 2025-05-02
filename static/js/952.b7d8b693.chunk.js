0b14c7c58714e5ad095a870038c29e0d1b39c878
Mikael Gorsky <mikaelgorsky@Mikaels-MacBook-Air.local>
2025-05-02 21:04:15 +0300

    Updates

---
+++ static/js/952.b7d8b693.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkemotibot_new=self.webpackChunkemotibot_new||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.b7d8b693.chunk.js.map
